3ce88b546ffca5a6b3487d9befc01efd2b95b6b3
CodeInDna <ykarelia323@gmail.com>
2019-12-26 16:53:51 +0530

    Exploring commonly used Datasets

---
+++ R_Basics/04_SampleDatasets.R
@@ -0,0 +1,33 @@
+# File: 02_Sample_Datasets.R
+# Project: Baiscs_R
+
+# INSTALL AND LOAD PACKAGES ##################
+
+
+# Load base packages manually
+library(datasets)
+?datasets
+library(help = "datasets")
+
+
+# SOME SAMPLE DATASETS #########################
+
+# iris data
+?iris
+iris
+
+# UCBAdmissions
+?UCBAdmissions
+UCBAdmissions
+
+# Titanic
+?Titanic
+Titanic
+
+# state.x77
+?state.x77
+state.x77
+
+# state.x77
+?swiss
+swiss
